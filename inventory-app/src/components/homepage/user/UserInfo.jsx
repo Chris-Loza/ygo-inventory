c750491586e9329e29b95a73923494badb80da8c
Chris-Loza <christianloza99@gmail.com>
2024-09-27 14:46:46 -0700

    need to implement user information pulling properly

--- inventory-app/src/components/homepage/user/UserInfo.jsx
+++ inventory-app/src/components/homepage/user/UserInfo.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./userInfo.css";
-import { auth } from "../../../lib/firebase";
+import { auth, db } from "../../../lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const UserInfo = () => {
+  // const [currentUser, setCurrentUser] = useState({});
+  // console.log(auth.currentUser.uid);
+
+  // useEffect(() => {
+  //   const fetchUserInfo = async (uid) => {
+  //     const docRef = doc(db, "users", uid);
+  //     const docSnap = await getDoc(docRef);
+
+  //     console.log(docSnap.data());
+  //     setCurrentUser(docSnap.data());
+  //   };
+
+  //   return () => {
+  //     fetchUserInfo(auth?.currentUser.uid);
+  //   };
+  // }, [auth.currentUser]);
+
+  // console.log(currentUser);
+
   return (
     <div className="userInfo">
       <div className="user">
-        <img src="../../../images/UserIconNoFill.svg" alt="User Icon" />
-        <h3>Username</h3>
+        <img src={"../../../images/UserIconNoFill.svg"} alt="User Icon" />
+        <h3>username</h3>
       </div>
       <button onClick={() => auth.signOut()}>Sign Out</button>
     </div>
