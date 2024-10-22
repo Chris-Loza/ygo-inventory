3022680d8061e4f0b415efdf8b0c298cf8ee0180
Chris-Loza <christianloza99@gmail.com>
2024-10-22 12:09:15 -0700

    added edit profile functionality

--- inventory-app/src/components/homepage/user/UserInfo.jsx
+++ inventory-app/src/components/homepage/user/UserInfo.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
 import "./userInfo.css";
 import { auth, db } from "../../../lib/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import upload from "../../../lib/upload";
+import { toast } from "react-toastify";
 
 const UserInfo = () => {
   const [currentUser, setCurrentUser] = useState({});
-  // console.log(auth.currentUser.uid);
+  const [updatedPicture, setUpdatedPicture] = useState({
+    file: null,
+    url: "",
+  });
 
   useEffect(() => {
-    if(!auth.currentUser) return;
+    if (!auth.currentUser) return;
     const fetchUserInfo = async (uid) => {
       const docRef = doc(db, "users", uid);
       const docSnap = await getDoc(docRef);
@@ -18,17 +23,55 @@ const UserInfo = () => {
     };
 
     return () => {
-      if(!auth.currentUser) return;
+      if (!auth.currentUser) return;
       fetchUserInfo(auth?.currentUser.uid);
     };
   }, [auth.currentUser]);
 
-  // console.log(currentUser);
+  const handleProfilePicChange = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setUpdatedPicture({
+        file: file,
+        url: URL.createObjectURL(file),
+      });
+
+      try {
+        const imageURL = await upload(file);
+
+        const docRef = doc(db, "users", auth?.currentUser.uid);
+        await updateDoc(docRef, {
+          avatar: imageURL,
+        });
+
+        setCurrentUser((prevState) => ({
+          ...prevState,
+          avatar: imageURL,
+        }));
+
+        toast.success("Profile Picture Changed!");
+      } catch (error) {
+        console.log(error);
+        toast.error(error.message);
+      }
+    }
+  };
 
   return (
     <div className="userInfo">
       <div className="user">
-        <img src={currentUser?.avatar || "../../../images/UserIconNoFill.svg"} alt="User Icon" />
+        <label htmlFor="file">
+          <img
+            src={currentUser?.avatar || "../../../images/UserIconNoFill.svg"}
+            alt="User Icon"
+          />
+        </label>
+        <input
+          type="file"
+          id="file"
+          style={{ display: "none" }}
+          onChange={handleProfilePicChange}
+        />
         <h3>{currentUser?.username}</h3>
       </div>
       <button onClick={() => auth.signOut()}>Sign Out</button>
